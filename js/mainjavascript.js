e94106388b89338c4b0da7f5365e2c195b7fa9eb
simone menichetti <simon3menichettui95@gmail.com>
2024-06-05 16:52:07 +0200

    inserimento const input + function variabile prezzokm * km utente

--- js/mainjavascript.js
+++ js/mainjavascript.js
@@ -6,6 +6,9 @@ let totale;
 // selezione input
 
 const kmUtente = document.querySelector(".inserimentokm");
+const ageUtente = document.querySelector("sceltaeta")
+const  prezzoKm = 0.21;
+
 
 // selezione pulsante
 
@@ -18,18 +21,27 @@ console.log("buttonGenera");
 
 console.log("il km scelto dall'utente è:",kmUtente);
 
-let
+// gestione interazione utente
+
+buttonGenera.addEventListener ("click",
+
+function () {
+  let inputValue = prezzoKm * kmUtente.value;
+  
+  console.log(inputValue);
+}
+);
 
 
-// chiedere l’età del passeggero.
+// // chiedere l’età del passeggero.
 
 // let ageUtente = parseInt(prompt("inserisci la tua età"));
 
 // console.log("l'età dell'utente è:", ageUtente);
 
 
-// // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// // il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// // // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// // // il prezzo del biglietto è definito in base ai km (0.21 € al km)
 // const  prezzoKm = 0.21;
 
 // let risultatoPrezzoKmBase = prezzoKm * kmUtente;
@@ -37,10 +49,10 @@ let
 // console.log("il risultato del prezzo viaggio tot è: ", risultatoPrezzoKmBase);
 
 
-// // va applicato uno sconto del 20% per i minorenni
-// const scontoMinorenni = 0.20;
-//  // va applicato uno sconto del 40% per gli over 65.
-// const scontoMaggiore = 0.40;
+// // // va applicato uno sconto del 20% per i minorenni
+// // const scontoMinorenni = 0.20;
+// //  // va applicato uno sconto del 40% per gli over 65.
+// // const scontoMaggiore = 0.40;
 
 // if (ageUtente < 18) {
   
@@ -62,7 +74,7 @@ let
 //     console.log("Il prezzo finale x sconto non applicato se si è >=18 è:",  risultatoPrezzoKmBase );
 //     totale = risultatoPrezzoKmBase;
 // }
-// L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+// // L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 
-// document.getElementById("prezzoutente").innerHTML ="Totale =  " + totale.toFixed(2) + " euro";
+// // document.getElementById("prezzoutente").innerHTML ="Totale =  " + totale.toFixed(2) + " euro";
 
